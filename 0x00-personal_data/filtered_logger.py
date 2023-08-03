e9795860839784066081b464d07959acd1baeffc
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-08-03 18:38:55 +0300

    'Log formatter' task

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,20 @@ Log message obfuscation
 
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+    def __init__(self, fields: List[str]):
+        """Constructor"""
+        super(RedactingFormatter, self).__init__()
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters values in incoming log records"""
+        return filter_datum(self.fields, 'xxxxx', super(
+            RedactingFormatter, self).format(record), ';')
 
 
 def filter_datum(fields: List[str], redaction: str,
