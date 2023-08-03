4b3f0abb9036b1be02a8d45e1944c4d4e5bc433f
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-08-03 19:25:33 +0300

    'Log formatter' task correction

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,19 +8,6 @@ from typing import List
 import logging
 
 
-class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class"""
-    def __init__(self, fields: List[str]):
-        """Constructor"""
-        super(RedactingFormatter, self).__init__()
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """Filters values in incoming log records"""
-        return filter_datum(self.fields, 'xxxxx', super(
-            RedactingFormatter, self).format(record), ';')
-
-
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Returns the log message obfuscated"""
@@ -28,3 +15,20 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Constructor"""
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters values in incoming log records"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
