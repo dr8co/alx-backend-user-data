b25f3954cbe5dfc5e7106f8b8a0e0ec81b987e7c
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-08-03 18:35:23 +0300

    'Regex-ing' task

---
+++ 0x00-personal_data/README.md
@@ -0,0 +1,5 @@
+# 0x00 - Personal Data
+
+## About
+
+A project on handling of users' personal data in a backend application.
---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Log message obfuscation
+"""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
