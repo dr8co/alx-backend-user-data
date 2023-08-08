a9a9f108e35f527905de3bc2221b04dead662f60
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-08-08 12:43:30 +0300

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Class to manage the API authentication.
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Manage the API authentication.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Public method to require authentication for all routes
+        except for excluded_paths.
+        """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Public method to validate if an authorization header is present
+        and contains the Base64 encoded value of the API key.
+        """
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Public method to overloads current_user - and
+        returns None for now.
+        """
+        return None
