e4b0cf7d67b6e5d302ce767ccda07d7cb8f9681a
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-08-08 13:05:33 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,6 +13,7 @@ class BasicAuth(Auth):
     """
     BasicAuth class to manage the API authentication.
     """
+
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """
@@ -25,3 +26,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """
+        Public method to return the decoded value of a Base64 string.
+        """
+        if base64_authorization_header is None or\
+                type(base64_authorization_header) is not str:
+            return None
+        try:
+            return b64decode(base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
