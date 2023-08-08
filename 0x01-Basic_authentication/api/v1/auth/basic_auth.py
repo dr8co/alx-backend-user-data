fe4573c538383542e100480c4bfcd7295f3bac98
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-08-08 12:56:41 +0300

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+BasicAuth class to manage the API authentication.
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class to manage the API authentication.
+    """
+    pass
